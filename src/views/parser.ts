21d481da9c91842ced19dbbc5958c09fde3ef254
guopenghui <guopeng_hui@126.com>
2022-10-23 23:22:39 +0800

    过滤掉文章中的中文字符，不再解析为新词

--- src/views/parser.ts
+++ src/views/parser.ts
@@ -149,7 +149,7 @@ export class TextParser {
                         ? STATUS_MAP[this.words.get(textLower).status]
                         : "new";
 
-                    return /^[0-9]+$/.test(text)  // 不把数字当做单词
+                    return /[0-9\u4e00-\u9fa5]/.test(text)  // 不把数字当做单词
                         ? `<span>${text}</span>`
                         : `<span class="word ${status}">${text}</span>`;
                 }
